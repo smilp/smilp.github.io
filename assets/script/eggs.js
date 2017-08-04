c685e9f082e5883b822253233a790be56e7d1409
SmilpPesko <30625992+smilp@users.noreply.github.com>
2017-08-04 23:07:23 +0800

    Add files via upload

---
+++ assets/script/eggs.js
@@ -0,0 +1,24 @@
+function addEgg() {
+    //创建一个div
+    var div = document.createElement('div');
+    //设置div的属性
+    div.id='eggdiv';
+    div.innerHTML = '<iframe src="https://ykilin.moe/smilp/egg.html" frameborder="no" style="position:absolute;top:0;bottom:0;left:0;right:0;width:100%;height:100%;max-width:600px;max-height:350px;display:block;margin:auto;text-align:center;border-radius:5px;"></iframe>';
+    div.style.background = "rgba(0,0,0,.85)";
+    div.style.position="fixed";
+    div.style.zIndex=10000;
+    div.style.top="0";
+    div.style.bottom="0";
+    div.style.left="0";
+    div.style.right="0";
+    div.onclick=function(){div.parentNode.removeChild(div);};
+    var bo = document.body; //获取body对象.
+    //动态插入到body中
+    bo.insertBefore(div, bo.lastChild);
+}
+function eggNotice1(){
+    alert("啊哈哈，是不是看到动的东西就很想点？可惜不是这里\n提示：仔细找吧，找到一个看似不能按却能按的地方，那就是了√");
+}
+function eggNotice2(){
+    alert("提示：那是一个生日礼物噢！");
+}
